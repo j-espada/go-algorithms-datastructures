6db2b2ce1728531b2426f6448ce449ea8b8cac12
j.espada <ctw00684@criticaltechworks.com>
2019-12-13 17:42:52 +0000

    counting sort not working

--- src/main.go
+++ src/main.go
@@ -6,6 +6,6 @@ import "./algorithms/sorting"
 func main() {
 	arr:= []int{9,8,7,6,5,4,3,2,1,0}
 	fmt.Println(arr)
-	var arrayS = sorting.InsertionSortInteger(arr, len(arr))
+	var arrayS = sorting.CountingSortInteger(arr, len(arr), 9)
 	fmt.Println(arrayS)
 }
