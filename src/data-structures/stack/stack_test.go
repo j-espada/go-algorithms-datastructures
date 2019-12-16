bb60c09cf6ea95742335f5b5c5490afa931faef9
j.espada <ctw00684@criticaltechworks.com>
2019-12-16 18:59:04 +0000

    test failing need to make this pass

--- src/data-structures/stack/stack_test.go
+++ src/data-structures/stack/stack_test.go
@@ -3,6 +3,7 @@ package stack
 import (
 	"fmt"
 	"testing"
+	"math/rand"
 )
 
 type NodeInterfaceImpl struct {
@@ -12,6 +13,13 @@ func (a NodeInterfaceImpl) String() string {
 	return fmt.Sprintln(a.Value)
 }
 
+func generateData(n int) [] *Node {
+	var data = make([] *Node, n)
+	for i := 0; i < n  ; i++  {
+		data[i] = &Node{Value:NodeInterfaceImpl{Value:rand.Int()} }
+	}
+	return data
+}
 
 func TestCreateStack(t *testing.T) {
 
@@ -22,9 +30,27 @@ func TestCreateStack(t *testing.T) {
 		t.Error("Expected ", stackCapacity, "got ",stack.capacity)
 	}
 
-
 	if stack.top != 0 {
 		t.Error("Expected 0", stackCapacity, "got ", stack.top)
 	}
+}
 
+func TestStack_Push(t *testing.T) {
+	var n = 10
+	var data = generateData(n)
+	var stack = CreateStack(n)
+	
+	for i:= 0; i < n; i++  {
+		stack.Push(data[i])
+	}
+
+	for i:= 0; i < n ;i ++  {
+		if stack.stack[i] != data[i] {
+			t.Error("Expected ", data[i], "got ", stack.stack[i])
+		}
+	}
+
+	if stack.top != n {
+		    t.Error()
+	}
 }
