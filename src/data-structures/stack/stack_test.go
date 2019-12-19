2885ca8190fc068c13fee804c070e3b8c05977df
joaquim espada <espadas.joaquim@gmail.com>
2019-12-19 18:29:14 +0000

    finished unit testing stack

--- src/data-structures/stack/stack_test.go
+++ src/data-structures/stack/stack_test.go
@@ -29,7 +29,18 @@ func populateStack(stack *Stack, data [] *Node) {
 	}
 }
 
-func TestCreateStack(t *testing.T) {
+func TestNode_String(t *testing.T) {
+	var value = 10
+	var stringOfValue = fmt.Sprintln(value)
+	var node = Node{Value: &NodeInterfaceImpl{Value: value}}
+	var nodeStr = node.String()
+	if stringOfValue != node.String() {
+		t.Error("Expected: ", stringOfValue, "got: ", nodeStr)
+
+	}
+}
+
+func TestStack_CreateStackWithDefinedCapacity(t *testing.T) {
 	stackCapacity := 10
 	stack := CreateStack(stackCapacity)
 
@@ -38,7 +49,23 @@ func TestCreateStack(t *testing.T) {
 	}
 
 	if stack.top != 0 {
-		t.Error("Expected: 0", stackCapacity, "got: ", stack.top)
+		t.Error("Expected: 0", stack.capacity, "got: ", stack.top)
+	}
+
+	if stack.IsEmpty() != true {
+		t.Error("Expected: true", "got: false")
+	}
+}
+
+func TestStack_CreateStackWithDefaultCapacity(t *testing.T) {
+	stack := CreateStackDefaultCapacity()
+
+	if stack.capacity != DefaultSize {
+		t.Error("Expected: ", DefaultSize, "got: ", stack.capacity)
+	}
+
+	if stack.top != 0 {
+		t.Error("Expected: 0", stack.capacity, "got: ", stack.top)
 	}
 
 	if stack.IsEmpty() != true {
@@ -49,11 +76,15 @@ func TestCreateStack(t *testing.T) {
 func TestStack_Push(t *testing.T) {
 	var n = 10
 	var data = generateData(n)
-	var stack = CreateStack(n+1)
+	var stack = CreateStack(n)
 	populateStack(stack, data)
 	for i:= 0; i < n ;i ++  {
 		if stack.stack[i] != data[i] {
 			t.Error("Expected: ", data[i], "got: ", stack.stack[i])
+		}
+
+		if stack.top != i {
+
 		}
 	}
 
@@ -63,9 +94,48 @@ func TestStack_Push(t *testing.T) {
 }
 
 func TestStack_IsEmpty(t *testing.T) {
+	var n = 10
+	var stack = CreateStack(n)
+
+	if stack.IsEmpty() != true {
+		t.Error("Expected: ", true , "got: ", false)
+	}
 
 }
 
 func TestStack_Pop(t *testing.T) {
+	var n = 10
+	var data = generateData(n)
+	var stack = CreateStack(n+1)
+	populateStack(stack, data)
 
+	for stack.IsEmpty() == false {
+		stack.Pop()
+	}
+
+	if stack.IsEmpty() != true {
+		t.Error("Expected: ", true , "got: ", false)
+
+	}
+}
+
+func TestStack_EmptyStackGotNil(t *testing.T) {
+	var n = 10
+	var stack = CreateStack(n)
+	var pop = stack.Pop()
+	if pop != nil {
+		t.Error("Expected: ", nil , "got: ", pop)
+	}
+}
+
+func TestStack_DuplicateSize(t *testing.T) {
+
+	var n = 10
+	var stack = CreateStack(n)
+	var stackCap = stack.capacity
+	stack.duplicateSizeAndCopy()
+
+	if stack.capacity != stackCap*2 {
+		t.Error("Expected: ", stack.capacity , "got: ", stackCap*2)
+	}
 }
